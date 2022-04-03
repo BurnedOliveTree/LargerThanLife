58aba3633fd9608a4dd80b11b8eebd099b040be0
Rolkarolka <romanowskarolina@gmail.com>
2022-04-03 09:42:59 +0200

    scale bitmap

--- scenes/game.py
+++ scenes/game.py
@@ -6,13 +6,15 @@ class Game:
         self.window_size = window_size
 
     def get_surface_from_bitmap(self, bitmap):
-        scaled_bitmap = 255*bitmap
-        return pygame.surfarray.make_surface(scaled_bitmap)
+        scaled_color_bitmap = 255*bitmap
+        bitmap_surface = pygame.surfarray.make_surface(scaled_color_bitmap)
+        scaled_bitmap_surface = pygame.transform.scale(bitmap_surface, (self.window_size, self.window_size))
+        return scaled_bitmap_surface
         
     def render(self, screen, clock, FPS):
         while True:
             clock.tick(FPS)
-            bitmap = np.round(np.random.random((self.window_size, self.window_size)))
+            bitmap = np.round(np.random.random((self.window_size//4, self.window_size//4))) #to check if scaling works
             background = self.get_surface_from_bitmap(bitmap)
             screen.blit(background, (0, 0))
             pygame.display.update()
