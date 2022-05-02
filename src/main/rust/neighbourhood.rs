dd412e3f19137c52f98e4d64f9b57a5882071d36
Rolkarolka <romanowskarolina@gmail.com>
2022-05-02 20:44:24 +0200

    append tests for neighbourhood

--- src/main/rust/neighbourhood.rs
+++ src/main/rust/neighbourhood.rs
@@ -20,3 +20,34 @@ impl Neighbourhood {
         }
     }
 }
+
+// Tests -------------------------------------------------------------------------------------------
+
+#[cfg(test)] 
+mod tests {
+    use super::*;
+    #[test]
+    fn test_from_str_moore() {
+        assert_eq!(
+            Neighbourhood::from_str("M"), 
+            Ok(Neighbourhood::Moore)
+        );
+    }
+
+    #[test]
+    fn test_from_str_neumann() {
+        assert_eq!(
+            Neighbourhood::from_str("N"), 
+            Ok(Neighbourhood::VonNeumann)
+        );
+    }
+
+    #[test]
+    fn test_from_str_other_string() {
+        assert_eq!(
+            Neighbourhood::from_str("X"), 
+            Err(String::from("Tried to parse X as a neighbourhood type."))
+        );
+    }
+}
+
